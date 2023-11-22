9095dc7c7b428183b3be8f3bcbce3e4a19ec4a22
ext.andreperez <ext.andreperez@pjenl.gob.mx>
2023-11-22 16:16:17 -0600

    changed env var

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: 'http://localhost:4200/',
-  apiUrl: 'http://localhost:8000/api/',
-  //apiUrl: 'https://chatbot-v2-api.onrender.com/api/',
+  //apiUrl: 'http://localhost:8000/api/',
+  apiUrl: 'https://chatbot-v2-api.onrender.com/api/',
 };
