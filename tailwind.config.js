6d045de9ae54f65258c3cea38f8763a36a3c777b
ext.andreperez <ext.andreperez@pjenl.gob.mx>
2023-11-22 17:12:21 -0600

    style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ff002c",
+          secondary: "#f4002a",
+          accent: "#ff580a",
+          neutral: "#2f2f2f",
+          "base-100": "#ffffff",
+          info: "#1d5bc2",
+          success: "#25d366",
+          warning: "#ff5700",
+          error: "#76001c",
+        },
+      },
+    ],
+  },
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui"), require("tailwindcss-animate")],
-}
-
+};
